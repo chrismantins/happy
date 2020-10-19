f2fa82139e66d39eeee52fb82ab4ecb12541135a
Christian <br>
2020-10-19 19:44:22 -0300

    version 1.0 complete

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiArrowRight, FiPlus } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+import mapMarkerImg from "../images/map-marker.svg";
+
+import "../styles/pages/orphanages-map.css";
+import mapIcon from "../utils/mapIcon";
+import api from "../services/api";
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
+function OrphanagesMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get("orphanages").then((response) => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
+  return (
+    <div id='page-map'>
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt='Happy' />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Guarulhos</strong>
+          <span>São Paulo</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-23.4578098, -46.5182986]}
+        zoom={13}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer url='http://a.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+
+        {orphanages.map((orphanage) => {
+          return (
+            <Marker
+              key={orphanage.id}
+              position={[orphanage.latitude, orphanage.longitude]}
+              icon={mapIcon}
+            >
+              <Popup
+                closeButton={false}
+                minWidth={240}
+                maxWidth={240}
+                className='map-popup'
+              >
+                {orphanage.name}
+                <Link to={`orphanages/${orphanage.id}`}>
+                  <FiArrowRight size={20} color={"#fff"} />
+                </Link>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </Map>
+
+      <Link to='orphanages/create' className='create-orphanage'>
+        <FiPlus size={24} color='#fff' />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
---
+++ web/src/utils/mapIcon.ts
@@ -0,0 +1,13 @@
+import Leaftet from "leaflet";
+
+import mapMarkerImg from "../images/map-marker.svg";
+
+const mapIcon = Leaftet.icon({
+  iconUrl: mapMarkerImg,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60],
+});
+
+export default mapIcon;
